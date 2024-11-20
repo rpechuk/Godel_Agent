8e15ee2e9529318df90e3dd2a20567bc9b59a629
Ron Pechuk <rpechuk@gmail.com>
2024-11-19 19:13:35 -0800

    models added

--- models/create-models.bash
+++ models/create-models.bash
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 # Load the model into Ollama
-ollama create gpt-4o -f "./gpt-4o-Modelfile"
+ollama create gpt-4o -f "./modelfile"
 # Load the model into Ollama
-ollama create gpt-4o-mini -f "./gpt-4o-Modelfile"
+ollama create gpt-4o-mini -f "./modelfile"
 # Load the model into Ollama
 ollama pull llama3.2
 ollama cp llama3.2 gpt-3.5-turbo
+echo "Model creation complete"
+# Load the embedding into Ollama
+ollama pull nomic-embed-text
+echo "Embedding creation complete"
 # List the models in Ollama
 ollama list
